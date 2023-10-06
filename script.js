c8c527f4ac76ffcf62ea9d42e716af0ac24dab58
lhgioia <lhgioia@gmail.com>
2023-10-06 12:54:32 -0700

    Update with button, module, and factory

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
-const myLibrary = [];
+// // global library
+// const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.readText = read ? "already read" : "not read yet";
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${this.readText}`;
+// // constructor
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+//   this.readText = read ? "already read" : "not read yet";
+//   this.info = function () {
+//     return `${title} by ${author}, ${pages} pages, ${this.readText}`;
+//   };
+// }
+
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+// console.log(theHobbit.info());
+
+// function addBookToLibrary() {
+//   let bookToAdd = prompt("Add a book title", "Boook");
+//   myLibrary.push(bookToAdd);
+// }
+
+// addBookToLibrary();
+
+// console.log(myLibrary);
+
+// use innerText to display library
+
+// factory function
+const Book = (title, author, pages, read) => {
+  const readText = read ? "already read" : "not read yet";
+  const info = () => {
+    return `${title} by ${author}, ${pages} pages, ${readText}`;
   };
-}
+  return {
+    title,
+    author,
+    pages,
+    readText,
+    info,
+  };
+};
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+// module
+const library = (() => {
+  bookList = [];
 
-console.log(theHobbit.info());
+  const addButton = document.querySelector("button");
+  const libraryDiv = document.querySelector("div");
 
-function addBookToLibrary() {
-  let bookToAdd = prompt("Add a book title", "Boook");
-  myLibrary.push(bookToAdd);
-}
+  const addBook = () => {
+    let bookToAdd = prompt("Add a book title", "Boook");
+    bookList.push(bookToAdd);
+    libraryDiv.innerHTML = bookList;
+  };
 
-addBookToLibrary();
+  addButton.addEventListener("click", addBook);
 
-console.log(myLibrary);
+  removeBook = (book) => {};
+
+  return {
+    addBook,
+    removeBook,
+  };
+})();
